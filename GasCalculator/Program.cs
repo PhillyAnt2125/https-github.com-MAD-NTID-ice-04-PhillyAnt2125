c8cb131ade8a6f84c76655f0a1c4612b51fd4e90
PhillyAnt2125 <89260022+PhillyAnt2125@users.noreply.github.com>
2022-01-19 17:20:43 -0500

    Initial check-in of module GasCalculator

---
+++ GasCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GasCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double regular = 2.19;
+            double premium = 2.59;
+            const int NUMBER_OF_GALLONS = 14;
+            double totalRegularGasCost = NUMBER_OF_GALLONS * regular;
+            double totalPremiumGasCost = NUMBER_OF_GALLONS * premium;
+
+            Console.WriteLine("Regular Gas Cost: {0:C} Premium Gas Cost: {1:C}", totalRegularGasCost, totalPremiumGasCost);
+        }
+    }
+}
