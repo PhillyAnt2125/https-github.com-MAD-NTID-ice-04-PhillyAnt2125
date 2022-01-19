21713a7fa489ce49cdf96df0f38f36546da1022e
Anthony Caccese <89260022+PhillyAnt2125@users.noreply.github.com>
2022-01-19 17:34:48 -0500

    Add files via upload

---
+++ https-github.com-MAD-NTID-ice-04-PhillyAnt2125/ICE04/ICE04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ICE04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius = 20;
+            const int FREEZING_POINT = 32;
+            double fahrenheit = ((9 * celsius) / 5) + FREEZING_POINT;
+
+            Console.WriteLine(fahrenheit); //68
+        }
+    }
+    }
+}
