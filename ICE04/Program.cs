4ceffdf5ad74f97ac34ff6e660eb1b5b7ab3599d
PhillyAnt2125 <89260022+PhillyAnt2125@users.noreply.github.com>
2022-01-19 17:22:08 -0500

    Initial check-in of module ICE04

---
+++ ICE04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ICE04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius = 20;
+            const int FREEZING_POINT = 32;
+            double fahrenheit = ((9 * celsius) / 5) + FREEZING_POINT;
+
+            Console.WriteLine(fahrenheit); //68
+        }
+    }
+}
